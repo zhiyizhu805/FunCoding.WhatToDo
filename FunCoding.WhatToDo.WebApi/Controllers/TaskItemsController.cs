24b6903a8b09d5dc6305b4a296e34a633f947070
zhiyizhu <zhiyi.zhu@hotmail.com>
2025-01-27 15:02:30 +1300

    update variable name from task to taskItem

--- FunCoding.WhatToDo.WebApi/Controllers/TaskItemsController.cs
+++ FunCoding.WhatToDo.WebApi/Controllers/TaskItemsController.cs
@@ -19,60 +19,60 @@ public class TaskItemsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetTasksAsync(int pageIndex = 1, int pageSize = 20)
     {
-        var tasks = await _context.TaskItems.OrderBy(t => t.CreatedAt).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
-        return Ok(tasks);
+        var taskItems = await _context.TaskItems.OrderBy(t => t.CreatedAt).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+        return Ok(taskItems);
     }
 
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetTaskById(Guid id)
     {
-        var task = await _context.TaskItems.FindAsync(id);
-        if (task == null)
+        var taskItem = await _context.TaskItems.FindAsync(id);
+        if (taskItem == null)
         {
             return NotFound();
         }
-        return Ok(task);
+        return Ok(taskItem);
     }
 
 
     [HttpPost]
     public async Task<IActionResult> CreateTask(TaskItem newTaskItem)
     {
-        var task = new TaskItem
+        var taskItem = new TaskItem
         {
             Title = newTaskItem.Title,
             Description = newTaskItem.Description
         };
-        _context.TaskItems.Add(task);
+        _context.TaskItems.Add(taskItem);
         await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(CreateTask), new { id = task.Id }, task);
+        return CreatedAtAction(nameof(CreateTask), new { id = taskItem.Id }, taskItem);
     }
 
     [HttpPut]
-    public async Task<IActionResult> UpdateTask(TaskItem taskItem)
+    public async Task<IActionResult> UpdateTask(TaskItem updateTaskItem)
     {
-        var task = await _context.TaskItems.FindAsync(taskItem.Id);
-        if (task == null)
+        var taskItem = await _context.TaskItems.FindAsync(updateTaskItem.Id);
+        if (taskItem == null)
         {
             return NotFound();
         }
-        task.Title = taskItem.Title;
-        task.Description = taskItem.Description;
-        task.Status = taskItem.Status;
+        taskItem.Title = updateTaskItem.Title;
+        taskItem.Description = updateTaskItem.Description;
+        taskItem.Status = updateTaskItem.Status;
         await _context.SaveChangesAsync();
-        return Ok(task);
+        return Ok(taskItem);
     }
 
     [HttpDelete("{id:guid}")]
     public async Task<IActionResult> DeleteTaskById(Guid id)
     {
-        var task = await _context.TaskItems.FindAsync(id);
-        if (task == null)
+        var taskItem = await _context.TaskItems.FindAsync(id);
+        if (taskItem == null)
         {
             return NotFound();
         }
 
-        _context.TaskItems.Remove(task);
+        _context.TaskItems.Remove(taskItem);
         await _context.SaveChangesAsync();
         return Ok();
 
