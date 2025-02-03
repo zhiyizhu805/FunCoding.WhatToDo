5bf260ba33d093de137f4f5e6486768375473d6a
zhiyizhu <zhiyi.zhu@hotmail.com>
2025-02-03 23:54:36 +1300

    Added first unit test for GetTaskItems

---
+++ FunCoding.WhatToDo.UnitTests/TaskItemsControllerTests.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+using FunCoding.WhatToDo.WebApi.Controllers;
+using FunCoding.WhatToDo.WebApi.Interfaces;
+using FunCoding.WhatToDo.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FunCoding.WhatToDo.UnitTests;
+
+public class TaskItemsControllerTests
+{
+    [Fact]
+    public async Task GetTaskItems_ShouldReturn200Ok_WithTaskList_WhenRepositoryReturnsData()
+    {
+        //Arrange
+        var mockRepo = new Mock<ITaskItemRepository>();
+        var fakeTaskItems = new List<TaskItem>
+        {
+            new TaskItem
+            {
+                Title = "Fake title 1",
+                Description = "Fake Description 1"
+            },
+            new TaskItem
+            {
+                Title = "Fake title 2",
+                Description = "Fake Description 2"
+            }
+        };
+        mockRepo.Setup(repo => repo.GetTaskItemsAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(fakeTaskItems);
+        var controller = new TaskItemsController(mockRepo.Object);
+
+        //Act
+        var result = await controller.GetTaskItems();
+        //Assert
+        result.Should().BeOfType<OkObjectResult>().Which.StatusCode.Should().Be(200);
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().BeAssignableTo<IEnumerable<TaskItem>>().Which.Should().HaveCount(fakeTaskItems.Count)
+            .And.BeEquivalentTo(fakeTaskItems);
+        mockRepo.Verify(repo => repo.GetTaskItemsAsync(It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+    }
+
+}
